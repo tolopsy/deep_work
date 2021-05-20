eaa8e71f27c9fe7dd208e6ea0dd94271578529d5
Tolulope <lanre.toluwa@gmail.com>
2021-05-20 20:21:35 +0100

    Settings page layout completed

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:deep_work/widgets.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -7,9 +8,7 @@ class SettingsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Settings"),
         ),
-        body: Container(
-          child: Text("My Settings"),
-        ));
+        body: Settings());
   }
 }
 
@@ -21,8 +20,45 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(fontSize: 24.0);
+    Color buttonColor = Colors.deepPurple;
     return Container(
-      
-    );
+        child: GridView.count(
+      scrollDirection: Axis.vertical,
+      crossAxisCount: 3,
+      childAspectRatio: 3,
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      children: <Widget>[
+        Text("Work", style: textStyle),
+        Text(""),
+        Text(""),
+        SettingsButton(buttonColor, "-", -1),
+        TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number),
+        SettingsButton(buttonColor, "+", 1),
+        Text("Short", style: textStyle),
+        Text(""),
+        Text(""),
+        SettingsButton(buttonColor, "-", -1),
+        TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number),
+        SettingsButton(buttonColor, "+", 1),
+        Text("Long", style: textStyle),
+        Text(""),
+        Text(""),
+        SettingsButton(buttonColor, "-", -1),
+        TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number),
+        SettingsButton(buttonColor, "+", 1),
+      ],
+      padding: EdgeInsets.all(20.0),
+    ));
   }
 }
